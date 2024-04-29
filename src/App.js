343d550e9bf45e06df093e484b7d06740d373d04
megancurry514 <mrc5@njit.edu>
2024-04-28 22:22:06 -0400

    added correlation matrix

--- src/App.js
+++ src/App.js
@@ -2,11 +2,27 @@ import React, { Component } from "react";
 import tipData from './data/tips.csv';
 import * as d3 from "d3";
 
+function correlationCoefficient(array1, array2) {
+  const mean1 = d3.mean(array1);
+  const mean2 = d3.mean(array2);
+  let sum = 0;
+  let sum1 = 0;
+  let sum2 = 0;
+  for (let i = 0; i < array1.length; i++) {
+    sum += (array1[i] - mean1) * (array2[i] - mean2);
+    sum1 += Math.pow(array1[i] - mean1, 2);
+    sum2 += Math.pow(array2[i] - mean2, 2);
+  }
+  return sum / Math.sqrt(sum1 * sum2);
+}
+
+
 class App extends Component {
   componentDidMount() {
     // Call the function to draw the chart after the SVG elements are rendered
     this.drawBarChart();
     this.scatterPlot();
+    this.drawCorrelationMatrix();
   }
 
   scatterPlot() {
@@ -59,6 +75,55 @@ class App extends Component {
     });
   }
   
+  drawCorrelationMatrix() {
+    d3.csv(tipData).then(data => {
+      const margin = { top: 30, right: 30, bottom: 30, left: 30 };
+      const width = 600 - margin.left - margin.right;
+      const height = 600 - margin.top - margin.bottom;
+
+      const svg = d3.select("#matrix")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+      const variables = ['total_bill', 'tip', 'size'];
+      const correlationMatrix = variables.map((variable1, i) =>
+        variables.map((variable2, j) => i === j ? 1 : correlationCoefficient(data.map(d => +d[variable1]), data.map(d => +d[variable2])))
+      );
+  
+      const colorScale = d3.scaleSequential(d3.interpolateBlues)
+        .domain([-1, 1]);
+  
+      const xScale = d3.scaleBand()
+        .domain(variables)
+        .range([0, width]);
+  
+      const yScale = d3.scaleBand()
+        .domain(variables)
+        .range([height, 0]);
+  
+      svg.selectAll()
+        .data(correlationMatrix.flat())
+        .enter()
+        .append("rect")
+        .attr("x", (d, i) => xScale(variables[i % variables.length]))
+        .attr("y", (d, i) => yScale(variables[Math.floor(i / variables.length)]))
+        .attr("width", xScale.bandwidth())
+        .attr("height", yScale.bandwidth())
+        .style("fill", d => colorScale(d));
+  
+      svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(xScale));
+
+      svg.append("g")
+        .call(d3.axisLeft(yScale));
+    }).catch(error => {
+      console.error('Error loading the data: ', error);
+    });
+  }
   
   drawBarChart() {
     d3.csv(tipData).then(data => {
